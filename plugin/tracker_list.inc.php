c53b77c930cba0138049a5ea78f6ee961c5f9e65
umorigu <umorigu@gmail.com>
2018-02-27 00:10:34 +0900

    BugTrack/560 tracker_list speeding up with cache
    * Implement 2-layer caching
      * Converted HTML cache
      * Contents cache for list pages
    * Get only updated/deleted page contents
    * Detect page updates by file or page timestamps
    * Refer cache/recent.dat file to detect updated pages
    * Refer RecentDeleted page to detect deleted pages
    * Handle page links properly on tracker_list table
    * Save contents cache on cache/(TrackerBase).tracker
    
    How to invalidate cache:
    
    Execute one of following actions.
    
    * Change tracker config file
    * Change tracker list file
    * Delete cache/*.tracker file manually

--- plugin/tracker_list.inc.php
+++ plugin/tracker_list.inc.php
@@ -1,7 +1,7 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone
 // tracker_list.inc.php
-// Copyright 2003-2017 PukiWiki Development Team
+// Copyright 2003-2018 PukiWiki Development Team
 // License: GPL v2 or (at your option) any later version
 //
 // Issue tracker list plugin (a part of tracker plugin)
